69c894145578153c30fe5c932675f5c62362b3ee
muleta <muletatezera@gmail.com>
2026-04-11 20:19:15 +0300

    feat: add

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,10 @@ function timeUpdate() {
 }
 timeUpdate();
 setInterval(timeUpdate, 1000);
+
+const time = new Date();
+const hours = time.getHours().toString().padStart(2, 0);
+const minutes = time.getMinutes().toString().padStart(2, 0);
+const seconds = time.getSeconds().toString().padStart(2, 0);
+const now = `${hours}:${minutes}:${seconds}`;
+document.getElementById("clock").innerText = now;
